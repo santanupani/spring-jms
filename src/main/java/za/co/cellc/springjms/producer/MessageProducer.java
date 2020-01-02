ed1d6a4cd313e4b1b0a85bd0fa89eb3485ff9fef
Za-cellcSPani <SPani@cellc.co.za>
2020-01-02 10:16:22 +0200

    welcome.html

--- src/main/java/za/co/cellc/springjms/producer/MessageProducer.java
+++ src/main/java/za/co/cellc/springjms/producer/MessageProducer.java
@@ -9,6 +9,7 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import za.co.cellc.springjms.model.MessageBean;
@@ -36,5 +37,10 @@ public class MessageProducer {
 		return null;
 		
 	}
+	
+	@RequestMapping("/")
+    public String welcome() {
+        return "login";
+    }
 
 }
