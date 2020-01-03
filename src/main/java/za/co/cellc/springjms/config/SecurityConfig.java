695c6e651b517cec5ef388ce029209193e8e1288
Za-cellcSPani <SPani@cellc.co.za>
2020-01-03 12:15:58 +0200

    pom

--- src/main/java/za/co/cellc/springjms/config/SecurityConfig.java
+++ src/main/java/za/co/cellc/springjms/config/SecurityConfig.java
@@ -1,5 +1,49 @@
 package za.co.cellc.springjms.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/").permitAll()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/login")
+				.permitAll()
+				.and()
+			.logout()
+				.permitAll();
+		
+	}
+
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user =				
+				User.withUsername("user")
+					.password("password")
+					.roles("USER")
+					.build();
+				
+		return new InMemoryUserDetailsManager(user);
+	}
+	
+	
 
 }
