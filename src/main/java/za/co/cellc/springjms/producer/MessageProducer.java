3ada72fb3e8f079aa01e0eb30dbe43b5bbc562d5
Za-cellcSPani <SPani@cellc.co.za>
2020-01-03 10:27:47 +0200

    welcome

--- src/main/java/za/co/cellc/springjms/producer/MessageProducer.java
+++ src/main/java/za/co/cellc/springjms/producer/MessageProducer.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +17,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import za.co.cellc.springjms.model.MessageBean;
 
-@Controller
+@RestController
 public class MessageProducer {
 	
 	@Autowired
